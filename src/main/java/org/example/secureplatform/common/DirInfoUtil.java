ccb66c3813e88b030548887f8c354862e6e91bd9
Pickkjouska <104264600+Pickkjouska@users.noreply.github.com>
2024-12-17 20:44:32 +0800

    文件 更新文件mode

--- src/main/java/org/example/secureplatform/common/DirInfoUtil.java
+++ src/main/java/org/example/secureplatform/common/DirInfoUtil.java
@@ -7,15 +7,46 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.FileOwnerAttributeView;
-import java.nio.file.attribute.GroupPrincipal;
-import java.nio.file.attribute.PosixFileAttributes;
-import java.nio.file.attribute.UserPrincipal;
+import java.nio.file.attribute.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class DirInfoUtil {
+    public static String toOctalMode(Set<PosixFilePermission> permissions) {
+        int padding = 0;
+        int owner = 0;
+        int group = 0;
+        int others = 0;
+
+        // 遍历权限集，将每个权限对应的标志转换为位
+        for (PosixFilePermission permission : permissions) {
+            if (permission.equals(PosixFilePermission.OWNER_READ)) {
+                owner |= 4;  // rwx -> 4
+            } else if (permission.equals(PosixFilePermission.OWNER_WRITE)) {
+                owner |= 2;  // rwx -> 2
+            } else if (permission.equals(PosixFilePermission.OWNER_EXECUTE)) {
+                owner |= 1;  // rwx -> 1
+            } else if (permission.equals(PosixFilePermission.GROUP_READ)) {
+                group |= 4;
+            } else if (permission.equals(PosixFilePermission.GROUP_WRITE)) {
+                group |= 2;
+            } else if (permission.equals(PosixFilePermission.GROUP_EXECUTE)) {
+                group |= 1;
+            } else if (permission.equals(PosixFilePermission.OTHERS_READ)) {
+                others |= 4;
+            } else if (permission.equals(PosixFilePermission.OTHERS_WRITE)) {
+                others |= 2;
+            } else if (permission.equals(PosixFilePermission.OTHERS_EXECUTE)) {
+                others |= 1;
+            }
+        }
+        // 拼接八进制表示
+        return String.format("%03o", padding) + String.format("%03o", owner) + String.format("%03o", group) + String.format("%03o", others);
+    }
+
+
     public static DirInfo getFileInfo(Path dirpath) {
         File folder = dirpath.toFile();
         DirInfo dirinfo = new DirInfo();
@@ -30,9 +61,9 @@ public class DirInfoUtil {
             UserPrincipal userPrincipal = ownerAttr.getOwner();
             dirinfo.setUser(userPrincipal.getName());
             // Linux下获取文件组信息
-            GroupPrincipal group = Files.readAttributes(dirpath, PosixFileAttributes.class).group();
-            dirinfo.setGroup(group.getName());
-//            dirinfo.setGroup("hhh");
+//            GroupPrincipal group = Files.readAttributes(dirpath, PosixFileAttributes.class).group();
+//            dirinfo.setGroup(group.getName());
+            dirinfo.setGroup("hhh");
             // 获取文件大小
             dirinfo.setSize(String.valueOf(Files.size(dirpath)));
             // 判断是否为符号链接
@@ -42,13 +73,14 @@ public class DirInfoUtil {
             /**
              * 链接路径
             **/
-            if (Objects.equals(dirinfo.getIsSymlink(), "true")) {
-                dirinfo.setLinkPath(Files.readSymbolicLink(dirpath));
-            }
+//            if (Objects.equals(dirinfo.getIsSymlink(), "true")) {
+//                dirinfo.setLinkPath(Files.readSymbolicLink(dirpath));
+//            }
             //权限等级
-            PosixFileAttributes attrs = Files.readAttributes(dirpath, PosixFileAttributes.class);
-            dirinfo.setMode(attrs.permissions().toString());
-//            dirinfo.setMode("0700");
+//            PosixFileAttributes attrs = Files.readAttributes(dirpath, PosixFileAttributes.class);
+//            dirinfo.setMode(toOctalMode(attrs.permissions()));
+
+            dirinfo.setMode("0700");
             // 获取文件的最后修改时间
             dirinfo.setUpdateTime(Files.getLastModifiedTime(dirpath).toString());
             // 判断是否为文件夹
